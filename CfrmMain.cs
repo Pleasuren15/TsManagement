691a54485a2caca15c55f0d297f09a0141b32ad7
Pleasuren15 <77783514+Pleasuren15@users.noreply.github.com>
2021-03-25 15:21:45 +0200

    New design & implementation

--- CfrmMain.cs
+++ CfrmMain.cs
@@ -8,27 +8,5 @@ namespace TsManagement
         {
             InitializeComponent();
         }
-        private void btnStockOut_Click(object sender, EventArgs e)
-        {
-        }
-        private void btnStockIn_Click(object sender, EventArgs e)
-        {
-        }
-        private void cmbStockFor_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-        private void cmbSortBy_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-        private void btnAddCreditor_Click(object sender, EventArgs e)
-        {
-        }
-        private void btnUpdateCreditor_Click(object sender, EventArgs e)
-        {
-        }
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
     }
 }
